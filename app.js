e8baf12b3e5f305467875d855786af86ab0d25de
nicola00 <nicolajobs00@gmail.com>
2023-02-09 15:53:39 +0100

    FEAT btn click and two for

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('ciao')
+
+
+let numGriglie = 10
+let numCelle = numGriglie * numGriglie
+
+let grigliaElement = document.querySelector('.griglia')
+let btnElement = document.getElementById('start')
+
+
+console.log(numCelle, grigliaElement)
+
+btnElement.addEventListener('click', function ( ) {
+    for (let i = 1; i <= numCelle; i++){
+    numCorrente = i
+    let cellaElement = `<div class="cella" style = " width: calc(100% / ${numGriglie});" >${numCorrente}</div>`
+
+    grigliaElement.innerHTML += cellaElement
+
+    }
+
+    let celleEl = document.querySelectorAll('.cella')
+
+    for (let i = 0; i < celleEl.length; i++) {
+        const cell = celleEl[i]
+
+        cell.addEventListener('click', function () {
+            console.log(i + 1)
+        })
+
+    }
+})
+
+
